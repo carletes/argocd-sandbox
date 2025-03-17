88ca08909f298689982a361c1188e143f69ca050
Carlos Valiente <carlos@pepelabs.net>
2025-03-17 19:12:04 +0100

    Local cluster.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            k3d
+            kubectl
+            yq
+          ];
+
+          shellHook = ''
+            export KUBECONFIG="$(pwd)/cluster/kubeconfig.yaml"
+          '';
+        };
+      };
+    };
+}
